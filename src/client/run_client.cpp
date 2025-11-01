ffea50a1ddb4d490178b9cc5b839a8be8315d565
tomludbrook <54624069+tomludbrook10@users.noreply.github.com>
2025-11-01 15:58:02 +1300

    changed address

--- src/client/run_client.cpp
+++ src/client/run_client.cpp
@@ -1,7 +1,7 @@
 #include "teleoperation_client.h"
 
 int main() {
-    TeleoperationClient client("192.168.99.48", "192.168.99.254");
+    TeleoperationClient client("192.168.99.201", "192.168.99.254");
     client.Start();
     return 0;
 }
